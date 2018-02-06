64a6a5df38e871be89539ef5e47b7bc20b6de347
Mauro Trevisan <mauro.simioni@targaubiest.com>
2018-02-06 17:39:18 +0100

    no message

--- run.bat
+++ run.bat
@@ -59,5 +59,4 @@ rem java -jar dist/CivilRecords.jar -s FS -a 61903/3:1:3QS7-L9ML-LD23 -u abcdef3
 rem java -jar dist/CivilRecords.jar -s FS -a 61903/3:1:33SQ-G5ZF-WN3 -u abcdef3 -p fermi123 -o "D:\Mauro\Treviso Nati 1913.pdf"
 
 
-java -jar dist/CivilRecords.jar -s FS -a 61903/3:1:3Q9M-CSVS-W1WS -u abcdef3 -p fermi123 -o "D:\Mauro\_Vera Cruz birth 194004-194005.pdf"
-java -jar dist/CivilRecords.jar -s FS -a 61903/3:1:3Q9M-CSV8-5ZQM -u abcdef3 -p fermi123 -o "D:\Mauro\_Vera Cruz birth 193708-193710.pdf"
+java -jar dist/CivilRecords.jar -s FS -a 61903/3:1:3Q9M-CS6C-DYLB -u abcdef3 -p fermi123 -o "D:\Mauro\_Vera Cruz marriage 193507-193610.pdf"
