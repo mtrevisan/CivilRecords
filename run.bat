ab3acb9522b6f2cbd0b0180615b4758ec3e931a8
Mauro Trevisan <mauro.simioni@targaubiest.com>
2019-05-23 13:41:31 +0200

    no message

--- run.bat
+++ run.bat
@@ -14,33 +14,33 @@ rem birth trevisan gelsomino eugenio 1925 #195
 rem tv morti 1914 gallinaro michele p. 225
 
 rem Silvestro Antonello, Angela Gonzato
-rem java -jar dist/CivilRecords.jar -s FS -a 61903/3:1:33S7-81WJ-4Q7 -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Oderzo Nati 1908.pdf"
+rem java -jar dist/CivilRecords.jar -s FS -a 61903/3:1:33S7-81WJ-4Q7 -u abcdef3 -p fermi123 -o "D:\Mauro\Oderzo Nati 1908.pdf"
 
 rem Luigi Trevisan + Maria di Dio (Breda? Santa Maria del rovere? after 14 jul 1900)
 
 
 rem examples
-rem java -jar dist/CivilRecords.jar -s SAN -a Archivio+di+Stato+di+Treviso/Stato+civile+napoleonico/Mogliano/Nati/1811/null/005009556_00176.jpg.html -o "C:\Users\mauro\Downloads\Mogliano Nati 1811.pdf"
-rem java -jar dist/CivilRecords.jar -s FS -a 61903/3:1:3QS7-L9ML-LD23 -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Fregona Nati 1887.pdf"
-rem java -jar dist/CivilRecords.jar -s FS -a 61903/3:1:33SQ-G5ZF-WN3 -o "C:\Users\mauro\Downloads\Treviso Nati 1913.pdf"
+rem java -jar dist/CivilRecords.jar -s SAN -a Archivio+di+Stato+di+Treviso/Stato+civile+napoleonico/Mogliano/Nati/1811/null/005009556_00176.jpg.html -o "D:\Mauro\Mogliano Nati 1811.pdf"
+rem java -jar dist/CivilRecords.jar -s FS -a 61903/3:1:3QS7-L9ML-LD23 -u abcdef3 -p fermi123 -o "D:\Mauro\Fregona Nati 1887.pdf"
+rem java -jar dist/CivilRecords.jar -s FS -a 61903/3:1:33SQ-G5ZF-WN3 -o "D:\Mauro\Treviso Nati 1913.pdf"
 
-rem java -jar dist/CivilRecords.jar -s SAN -a Archivio+di+Stato+di+Treviso/Stato+civile+napoleonico/San+Cassiano+di+Mesco/Nati/1808/338/005058553_00063.jpg.html -o "C:\Users\mauro\Downloads\San Cassiano del Meschio Nati 1808.pdf"
+rem java -jar dist/CivilRecords.jar -s SAN -a Archivio+di+Stato+di+Treviso/Stato+civile+napoleonico/San+Cassiano+di+Mesco/Nati/1808/338/005058553_00063.jpg.html -o "D:\Mauro\San Cassiano del Meschio Nati 1808.pdf"
 
-rem java -jar dist/CivilRecords.jar -s SAN -a Archivio+di+Stato+di+Udine/Stato+civile+napoleonico/Fratta+oggi+frazione+di+Caneva+ora+in+provincia+di+Pordenone/Matrimoni/1809/5068/005398138_00001.jpg.html -o "C:\Users\mauro\Downloads\Fratta Matrimoni 1809.pdf"
+rem java -jar dist/CivilRecords.jar -s SAN -a Archivio+di+Stato+di+Udine/Stato+civile+napoleonico/Fratta+oggi+frazione+di+Caneva+ora+in+provincia+di+Pordenone/Matrimoni/1809/5068/005398138_00001.jpg.html -o "D:\Mauro\Fratta Matrimoni 1809.pdf"
 
 
 rem Giuseppa Vittoria Trevisan
-rem java -jar dist/CivilRecords.jar -s FS -a 61903/3:1:33SQ-G5K7-FFC -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Treviso Nati 1887.pdf"
+rem java -jar dist/CivilRecords.jar -s FS -a 61903/3:1:33SQ-G5K7-FFC -u abcdef3 -p fermi123 -o "D:\Mauro\Treviso Nati 1887.pdf"
 rem Antonia Trevisan 20 jun
-rem java -jar dist/CivilRecords.jar -s FS -a 61903/3:1:33SQ-G55Z-CGN -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Treviso Nati 1920.pdf"
+rem java -jar dist/CivilRecords.jar -s FS -a 61903/3:1:33SQ-G55Z-CGN -u abcdef3 -p fermi123 -o "D:\Mauro\Treviso Nati 1920.pdf"
 
 rem Domenica Sonego
-rem java -jar dist/CivilRecords.jar -s SAN -a Archivio+di+Stato+di+Treviso/Stato+civile+napoleonico/Giavera/Nati/1810/402/005073374_00117.jpg.html -o "C:\Users\mauro\Downloads\Giavera Nati 1810.pdf"
+rem java -jar dist/CivilRecords.jar -s SAN -a Archivio+di+Stato+di+Treviso/Stato+civile+napoleonico/Giavera/Nati/1810/402/005073374_00117.jpg.html -o "D:\Mauro\Giavera Nati 1810.pdf"
 
-rem java -jar dist/CivilRecords.jar -s FS -a 61903/3:1:3QS7-99W2-WSN8 -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\San Stino di Livenza Matrimoni 1883-1916, 1919-1929 Morti 1871-1875.pdf"
-rem java -jar dist/CivilRecords.jar -s FS -a 61903/3:1:3QS7-89W2-W9FT -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\San Stino di Livenza Cittadinanze 1871-1929.pdf"
-rem java -jar dist/CivilRecords.jar -s FS -a 61903/3:1:3QS7-99W2-HKSH -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\San Stino di Livenza Morti 1876-1916, 1925, 1919-1929.pdf"
-rem java -jar dist/CivilRecords.jar -s FS -a 61903/3:1:3QSQ-G9W2-X33Z -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\San Stino di Livenza Matrimoni 1871-1882.pdf"
+rem java -jar dist/CivilRecords.jar -s FS -a 61903/3:1:3QS7-99W2-WSN8 -u abcdef3 -p fermi123 -o "D:\Mauro\San Stino di Livenza Matrimoni 1883-1916, 1919-1929 Morti 1871-1875.pdf"
+rem java -jar dist/CivilRecords.jar -s FS -a 61903/3:1:3QS7-89W2-W9FT -u abcdef3 -p fermi123 -o "D:\Mauro\San Stino di Livenza Cittadinanze 1871-1929.pdf"
+rem java -jar dist/CivilRecords.jar -s FS -a 61903/3:1:3QS7-99W2-HKSH -u abcdef3 -p fermi123 -o "D:\Mauro\San Stino di Livenza Morti 1876-1916, 1925, 1919-1929.pdf"
+rem java -jar dist/CivilRecords.jar -s FS -a 61903/3:1:3QSQ-G9W2-X33Z -u abcdef3 -p fermi123 -o "D:\Mauro\San Stino di Livenza Matrimoni 1871-1882.pdf"
 
 
 rem masutti vanda fb
@@ -50,40 +50,40 @@ rem nato canada emigrato 71 a sarmede
 
 
 rem examples
-rem java -jar dist/CivilRecords-2.3.0.jar -s SAN -a Archivio+di+Stato+di+Treviso/Stato+civile+napoleonico/Mogliano/Nati/1811/null/005009556_00176.jpg.html -o "C:\Users\mauro\Downloads\Mogliano Nati 1811.pdf"
-rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a 61903/3:1:3QS7-L9ML-LD23 -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Fregona Nati 1887.pdf"
-rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a 61903/3:1:33SQ-G5ZF-WN3 -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Treviso Nati 1913.pdf"
+rem java -jar dist/CivilRecords-2.3.0.jar -s SAN -a Archivio+di+Stato+di+Treviso/Stato+civile+napoleonico/Mogliano/Nati/1811/null/005009556_00176.jpg.html -o "D:\Mauro\Mogliano Nati 1811.pdf"
+rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a 61903/3:1:3QS7-L9ML-LD23 -u abcdef3 -p fermi123 -o "D:\Mauro\Fregona Nati 1887.pdf"
+rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a 61903/3:1:33SQ-G5ZF-WN3 -u abcdef3 -p fermi123 -o "D:\Mauro\Treviso Nati 1913.pdf"
 
 
 rem 1916: lina 555 I, valentino 486 II, lilliana 4a II
 
 rem Zanette*Dizionario del dialetto di Vittorio*.pdf
 rem 1948
-rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a 3Q9M-CSGG-Y4YV-9 -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Araraquara death 197104-197106.pdf"
-rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a 3Q9M-CSFH-LSZZ-7 -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Mococa death 194801-194905.pdf"
-java -jar dist/CivilRecords-2.3.0.jar -s FS -a 3Q9M-CSH2-CX5N -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Quintana birth 194011-194102.pdf"
-java -jar dist/CivilRecords-2.3.0.jar -s FS -a 3Q9M-CS6C-5W12 -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Quintana birth 194005-194008.pdf"
-rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Quintana birth .pdf"
-rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Quintana birth .pdf"
-rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Quintana birth .pdf"
-rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Quintana birth .pdf"
-rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Mococa birth .pdf"
-rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Mococa birth .pdf"
-rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Mococa birth .pdf"
-rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Mococa birth .pdf"
-rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Mococa birth .pdf"
-rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Mococa birth .pdf"
-rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Mococa birth .pdf"
-rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Mococa birth .pdf"
-rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Mococa birth .pdf"
-rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Mococa birth .pdf"
-rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Mococa birth .pdf"
-rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Mococa birth .pdf"
-rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Mococa birth .pdf"
-rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Mococa birth .pdf"
-rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Mococa birth .pdf"
-rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Mococa birth .pdf"
-rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Mococa birth .pdf"
-rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Mococa birth .pdf"
-rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Mococa birth .pdf"
-rem java -jar dist/CivilRecords-2.3.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "C:\Users\mauro\Downloads\Mococa birth .pdf"
+rem "C:\Program Files\Java\jdk-11.0.2\bin\java" -jar dist/CivilRecords-2.4.0.jar -s FS -a 3Q9M-CSGG-Y4YV-9 -u abcdef3 -p fermi123 -o "D:\Mauro\Araraquara death 197104-197106.pdf"
+rem "C:\Program Files\Java\jdk-11.0.2\bin\java" -jar dist/CivilRecords-2.4.0.jar -s FS -a 3Q9M-CSFH-LSZZ-7 -u abcdef3 -p fermi123 -o "D:\Mauro\Mococa death 194801-194905.pdf"
+"C:\Program Files\Java\jdk-11.0.2\bin\java" -jar dist/CivilRecords-2.4.0.jar -s FS -a 3Q9M-CSH2-CX5N -u abcdef3 -p fermi123 -o "D:\Mauro\Quintana birth 194011-194102.pdf"
+rem "C:\Program Files\Java\jdk-11.0.2\bin\java" -jar dist/CivilRecords-2.4.0.jar -s FS -a 3Q9M-CS6C-5W12 -u abcdef3 -p fermi123 -o "D:\Mauro\Quintana birth 194005-194008.pdf"
+rem "C:\Program Files\Java\jdk-11.0.2\bin\java" -jar dist/CivilRecords-2.4.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "D:\Mauro\Quintana birth .pdf"
+rem "C:\Program Files\Java\jdk-11.0.2\bin\java" -jar dist/CivilRecords-2.4.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "D:\Mauro\Quintana birth .pdf"
+rem "C:\Program Files\Java\jdk-11.0.2\bin\java" -jar dist/CivilRecords-2.4.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "D:\Mauro\Quintana birth .pdf"
+rem "C:\Program Files\Java\jdk-11.0.2\bin\java" -jar dist/CivilRecords-2.4.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "D:\Mauro\Quintana birth .pdf"
+rem "C:\Program Files\Java\jdk-11.0.2\bin\java" -jar dist/CivilRecords-2.4.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "D:\Mauro\Mococa birth .pdf"
+rem "C:\Program Files\Java\jdk-11.0.2\bin\java" -jar dist/CivilRecords-2.4.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "D:\Mauro\Mococa birth .pdf"
+rem "C:\Program Files\Java\jdk-11.0.2\bin\java" -jar dist/CivilRecords-2.4.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "D:\Mauro\Mococa birth .pdf"
+rem "C:\Program Files\Java\jdk-11.0.2\bin\java" -jar dist/CivilRecords-2.4.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "D:\Mauro\Mococa birth .pdf"
+rem "C:\Program Files\Java\jdk-11.0.2\bin\java" -jar dist/CivilRecords-2.4.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "D:\Mauro\Mococa birth .pdf"
+rem "C:\Program Files\Java\jdk-11.0.2\bin\java" -jar dist/CivilRecords-2.4.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "D:\Mauro\Mococa birth .pdf"
+rem "C:\Program Files\Java\jdk-11.0.2\bin\java" -jar dist/CivilRecords-2.4.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "D:\Mauro\Mococa birth .pdf"
+rem "C:\Program Files\Java\jdk-11.0.2\bin\java" -jar dist/CivilRecords-2.4.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "D:\Mauro\Mococa birth .pdf"
+rem "C:\Program Files\Java\jdk-11.0.2\bin\java" -jar dist/CivilRecords-2.4.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "D:\Mauro\Mococa birth .pdf"
+rem "C:\Program Files\Java\jdk-11.0.2\bin\java" -jar dist/CivilRecords-2.4.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "D:\Mauro\Mococa birth .pdf"
+rem "C:\Program Files\Java\jdk-11.0.2\bin\java" -jar dist/CivilRecords-2.4.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "D:\Mauro\Mococa birth .pdf"
+rem "C:\Program Files\Java\jdk-11.0.2\bin\java" -jar dist/CivilRecords-2.4.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "D:\Mauro\Mococa birth .pdf"
+rem "C:\Program Files\Java\jdk-11.0.2\bin\java" -jar dist/CivilRecords-2.4.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "D:\Mauro\Mococa birth .pdf"
+rem "C:\Program Files\Java\jdk-11.0.2\bin\java" -jar dist/CivilRecords-2.4.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "D:\Mauro\Mococa birth .pdf"
+rem "C:\Program Files\Java\jdk-11.0.2\bin\java" -jar dist/CivilRecords-2.4.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "D:\Mauro\Mococa birth .pdf"
+rem "C:\Program Files\Java\jdk-11.0.2\bin\java" -jar dist/CivilRecords-2.4.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "D:\Mauro\Mococa birth .pdf"
+rem "C:\Program Files\Java\jdk-11.0.2\bin\java" -jar dist/CivilRecords-2.4.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "D:\Mauro\Mococa birth .pdf"
+rem "C:\Program Files\Java\jdk-11.0.2\bin\java" -jar dist/CivilRecords-2.4.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "D:\Mauro\Mococa birth .pdf"
+rem "C:\Program Files\Java\jdk-11.0.2\bin\java" -jar dist/CivilRecords-2.4.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "D:\Mauro\Mococa birth .pdf"
+rem "C:\Program Files\Java\jdk-11.0.2\bin\java" -jar dist/CivilRecords-2.4.0.jar -s FS -a  -u abcdef3 -p fermi123 -o "D:\Mauro\Mococa birth .pdf"
